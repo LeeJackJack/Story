e37ac619e8b35d141cb93bf61b29c8d9f2909895
yaozhisheng <934741372@qq.com>
2023-08-24 17:38:57 +0800

    主页获取非预设图片成功

--- test_protagonist_controller.py
+++
@@ -1,8 +0,0 @@
-
-from main import app  # 从您的主应用文件中导入Flask应用
-from controllers.protagonist_controller import get_preset_role
-
-if __name__ == "__main__":
-    with app.app_context():  # 创建并使用应用程序上下文
-        result = get_preset_role()
-        print("Result:", result)
